69b99a816d5a07d68916831e942c0d827661b8c4
elvy <el.cayro@gmail.com>
2023-09-19 23:14:07 -0300

    aula de 20230919 que criou arquivos de formulario e contato e atributos. Tambem está incluindo dicas do Bing Chat para local host e landing page

---
+++ contato.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "<pre>";
+
+print_r($_POST);
+echo "</pre>";
+
+echo "<pre>";
+
+/* print_r($_GET);
+echo "</pre>";
+
+Array
+(
+    [nomeCompleto] => elvy bonzao
+    [email] => exemplo@email.com
+    [mail] => 
+    [numero] => 
+    [senha] => 
+    [arquivo] => 
+    [arquivos] => 
+    [estados] => SC
+    [id] => 1234
+) */
+
+?>
